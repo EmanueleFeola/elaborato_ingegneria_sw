2d1c0c9fba36687cbff2b059461ec35444fce403
Emanuele Feola <efeola99@gmail.com>
2020-05-31 12:24:33 +0200

    fixed new manager bug, added role choice box

--- view/ManagerEditDialogController.java
+++ view/ManagerEditDialogController.java
@@ -1,15 +1,13 @@
 package elaborato_ing_sw.view;
 
-//import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
-//import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
-import elaborato_ing_sw.model.Credentials;
 import elaborato_ing_sw.model.Manager;
 import elaborato_ing_sw.model.Role;
 
@@ -25,15 +23,15 @@ public class ManagerEditDialogController {
 	private TextField serialNumberField;
 	@FXML
 	private PasswordField passwordField;
-	//@FXML
-	//private ChoiceBox<Role> roleField;
+	
+	@FXML
+	private ChoiceBox<Role> roleField;
 
 	private Stage dialogStage;
-	private Manager manager;
+	private static Manager manager;
 	private boolean okClicked = false;
-	//private ObservableList<Role> roles;
 	
-	private int mode; //1 per new, 0 per edit
+	private boolean isNewMode; //1 per new, 0 per edit
 	
 	/**
 	 * Initializes the controller class. This method is automatically called after
@@ -52,30 +50,28 @@ public class ManagerEditDialogController {
 		this.dialogStage = dialogStage;
 	}
 	
-	public void setMode(int m) {
-		if (m == 1 || m == 0)
-			this.mode = m;
-		else
-			System.out.println("modalità non disponibile\n");
-	}
-
 	/**
 	 * Sets the person to be edited in the dialog.
 	 * 
 	 * @param person
 	 */
-	public void setManager(Manager manager) {
-		this.manager = manager;
-
+	public void setManager(Manager m) {
+		manager = m;
+		System.out.println("---");
+		System.out.println("manager: " + manager);
+		
+		roleField.getItems().setAll(Role.values());
+		
+		if(manager == null) {
+			isNewMode = true; 
+			return;
+		}
+		
 		firstNameField.setText(manager.getName());
 		lastNameField.setText(manager.getSurname());
 		dateOfBirthField.setValue(manager.getDateOfBirth());
-
-		if (mode == 0) {
-			serialNumberField.setText(Integer.toString(manager.getSerialNumber()));
-			passwordField.setDisable(true);
-		}
-		
+		serialNumberField.setText(Integer.toString(manager.getSerialNumber()));
+		passwordField.setDisable(true);
 	}
 
 	/**
@@ -92,23 +88,29 @@ public class ManagerEditDialogController {
 	 */
 	@FXML
 	private void handleOk() {
-		if (isInputValid()) {
+		if(!isInputValid())
+			return;
+		
+		if(manager == null) {
+			manager = new Manager(
+					firstNameField.getText(),
+					lastNameField.getText(),
+					dateOfBirthField.getValue(),
+					passwordField.getText(),
+					Integer.parseInt(serialNumberField.getText()), 
+					(Role)roleField.getValue()
+					);
+		} else {
 			manager.setName(firstNameField.getText());
 			manager.setSurname(lastNameField.getText());
 			manager.setDateOfBirth(dateOfBirthField.getValue());
-			manager.setSerialNumber(Integer.parseInt(serialNumberField.getText()));
-			
-			if (mode == 1) {		
-				manager.setCredentials(new Credentials(serialNumberField.getText(), passwordField.getText()));
-			} else {
-				manager.getCredentials().setUser(serialNumberField.getText());	
-			}
-			
-			manager.setRole(Role.ADMIN);
-
-			okClicked = true;
-			dialogStage.close();
+			manager.setSerialNumber(Integer.parseInt(serialNumberField.getText()));			
+			manager.getCredentials().setUser(serialNumberField.getText());	
+			manager.setRole((Role)roleField.getValue());
 		}
+
+		okClicked = true;
+		dialogStage.close();
 	}
 
 	/**
@@ -127,31 +129,26 @@ public class ManagerEditDialogController {
 	private boolean isInputValid() {
 		String errorMessage = "";
 
-		if (firstNameField.getText() == null || firstNameField.getText().length() == 0) {
+		if (firstNameField.getText() == null || firstNameField.getText().length() == 0)
 			errorMessage += "No valid first name!\n";
-		}
-		if (lastNameField.getText() == null || lastNameField.getText().length() == 0) {
+		if (lastNameField.getText() == null || lastNameField.getText().length() == 0)
 			errorMessage += "No valid last name!\n";
-		}
-		if (dateOfBirthField.getValue() == null) {
-			errorMessage += "No valid date of birth!\n";
-		}
-
-		if (serialNumberField.getText() == null || serialNumberField.getText().length() == 0) {
-			errorMessage += "No valid serial number!\n";
-		}
 		
-		if (mode == 1 && (passwordField.getText() == null || passwordField.getText().length() == 0)) {
+		if (dateOfBirthField.getValue() == null)
+			errorMessage += "No valid date of birth!\n";
+
+		if (serialNumberField.getText() == null || serialNumberField.getText().length() == 0)
+			errorMessage += "No valid serial number!\n";
+		
+		if (isNewMode && (passwordField.getText() == null || passwordField.getText().length() == 0))
 			errorMessage += "No valid password!\n";
-		}
 
-		/*if (roleField.getValue() == null) {
+		if (roleField.getValue() == null)
 			errorMessage += "No valid role!\n";
-		}*/
 
-		if (errorMessage.length() == 0) {
+		if (errorMessage.length() == 0)
 			return true;
-		} else {
+		else {
 			// Show the error message.
 			Alert alert = new Alert(AlertType.ERROR);
 			alert.initOwner(dialogStage);
@@ -164,4 +161,12 @@ public class ManagerEditDialogController {
 			return false;
 		}
 	}
+	
+	/**
+	 * Restituisce l'oggetto manager modificato oppure creato nuovo
+	 */
+	public static Manager getManager() {
+		return manager;
+	}
+	
 }
